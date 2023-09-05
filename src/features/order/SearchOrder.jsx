ece32ec894bcaa51978333cf12b3efd4093698da
Dmitry <antonovdmitry21@gmail.com>
2023-09-06 02:34:43 +0300

    Styling the app with Tailwind CSS

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState('')
-  const navigate = useNavigate()
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (!query) return
-    navigate(`/order/${query}`)
-    setQuery('')
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery('');
   }
 
   return (
@@ -19,9 +19,11 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="ficus:ring-opacity-50 w-28 rounded-full bg-yellow-100 px-4
+        py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 sm:w-64 sm:focus:w-72"
       />
     </form>
-  )
+  );
 }
 
-export default SearchOrder
+export default SearchOrder;
